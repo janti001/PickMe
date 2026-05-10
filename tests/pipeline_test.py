1f361cdfee7c50be50a1646158167c3e47f06a4d
Joseph Antinoro <jantinoro@Josephs-MacBook-Pro-2.local>
2026-05-10 12:43:37 +0100

    added particle_extraction to pipeline tester

--- tests/pipeline_test.py
+++ tests/pipeline_test.py
@@ -1,7 +1,10 @@
-from PickMe.main import extract_and_store, choose_object
+from PickMe.main import extract_and_store, choose_object, particle_extract
 from PickMe import utils, angles, filter, plotting, sampling
 
 extract_and_store(input_dir='/Users/jantinoro/Documents/LIDo/Rotation_2/python_projects/data/segmentation/')
 
 
 #choose_object(input_dir='/Users/jantinoro/Documents/LIDo/Rotation_2/python_projects/data/tomo_reconstruction')
+
+
+#particle_extract(grid_sampling=5, cmm=True)
