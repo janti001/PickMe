92b354ae7fdc012503d218b3e5004f05379f3ae2
Joseph Antinoro <jantinoro@Josephs-MacBook-Pro-2.local>
2026-05-10 11:45:44 +0100

    added output handling

--- src/PickMe/plotting/plot_angles.py
+++ src/PickMe/plotting/plot_angles.py
@@ -1,12 +1,18 @@
 import seaborn as sns 
 from matplotlib.pyplot import subplots
+from pathlib import Path
 from ..utils import check_make_dir
 
-def plot_angles(star_data):
+
+def plot_angles(star_data, output_dir, tomogram_name=None):
     '''
     Plots the distribution of the euler angles across all of the tomograms processed, to check if there are any biases in the distribution of angles which may affect downstream processing and alignment.
+    
     Rot should have a uniform distribution, while tilt and psi may have some bias depending on the shape of the membrane and the sampling, but we want to check if there are any extreme biases which may affect downstream processing.
+    
     :param star_data: the star dataframe containing the euler angles and other data for each particle
+    :param output_dir: directory win which the output of the job it is being called in. I.e., If it is being called in Job001 (which particle_extract) - this should be supplied.
+    :param tomogram_name: name of the tomogram file that is being processed. 
     :return: a plot of the distribution of the euler angles
     '''
     colors= sns.color_palette('viridis')
@@ -25,6 +31,7 @@ def plot_angles(star_data):
     ax[2].set_title('Distribution of Azimuthal Angles');
 
     #make plotting directory if it has not been made yet
-    output_directory = check_make_dir(job_name='plotting')
-
-    fig.savefig(f'{output_directory}/Angles_plot.png', format='png')
+    output_directory = Path(output_dir) / 'Angle_plots'
+    if tomogram_name is not None:
+        fig.savefig(f'{output_directory}/{tomogram_name}.png', format='png')
+    fig.savefig(f'{output_directory}/Angles_distribution.png', format='png')
