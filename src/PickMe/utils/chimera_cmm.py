5670b049ab95caddc67e5132739429538ccff937
Joseph Antinoro <jantinoro@Josephs-MacBook-Pro-2.local>
2026-05-11 14:06:31 +0100

    fixed cmm file writing

--- src/PickMe/utils/chimera_cmm.py
+++ src/PickMe/utils/chimera_cmm.py
@@ -1,5 +1,10 @@
+from pathlib import Path
+from xml.sax.saxutils import quoteattr
+
 import pandas as pd
-def cmm_write(data: pd.DataFrame, tomogram_name:str, output_directory, **kwargs):
+
+
+def cmm_write(data: pd.DataFrame, tomogram_name: str, output_directory, **kwargs):
     '''
     The particle coordinates and their normals will be written to a .cmm file which can be viewed in chimera
 
@@ -9,19 +14,23 @@ def cmm_write(data: pd.DataFrame, tomogram_name:str, output_directory, **kwargs)
 
     '''
     grid_sampling = kwargs['sampling']
-    with open (f'{output_directory}/{tomogram_name}.cmm', 'w') as cmm:
-        cmm.write(f'<marker_set name="{tomogram_name} @ {grid_sampling} sampling"')
+    output_path = Path(output_directory) / f'{tomogram_name}.cmm'
+    marker_set_name = f'{tomogram_name} @ {grid_sampling} sampling'
+    normal_id_offset = data.shape[0]
+
+    with open(output_path, 'w', encoding='utf-8') as cmm:
+        cmm.write(f'<marker_set name={quoteattr(marker_set_name)}>\n')
         for i in range(data.shape[0]):
             entry = data.iloc[i]
             x, y, z = entry['rlnCoordinateX'], entry['rlnCoordinateY'], entry['rlnCoordinateZ']
             nx, ny, nz = entry['rlnNormalX'], entry['rlnNormalY'], entry ['rlnNormalZ']
             pixel_size = entry['rlnImagePixelSize']
-            cmm.write(f'<marker id ="{i}" x="{x*pixel_size}" y="{y*pixel_size}" z="{z*pixel_size}" radius="1"/>\n')
-            cmm.write(f'<marker id="100{i}" x="{(x*pixel_size)+(pixel_size*nx)}" y="{(y*pixel_size)+(pixel_size*ny)}" z="{(z*pixel_size)+(pixel_size*nz)}" r="1" g="0" b="0" radius="1"/>\n')
-            cmm.write(f'<link id1="{i}" id2="100{i}" r="3" g="238" b="255" radius="0.5"/>\n')
+            normal_id = i + normal_id_offset
+            cmm.write(f'<marker id="{i}" x="{x*pixel_size}" y="{y*pixel_size}" z="{z*pixel_size}" radius="1"/>\n')
+            cmm.write(f'<marker id="{normal_id}" x="{(x*pixel_size)+(pixel_size*nx)}" y="{(y*pixel_size)+(pixel_size*ny)}" z="{(z*pixel_size)+(pixel_size*nz)}" r="1" g="0" b="0" radius="1"/>\n')
+            cmm.write(f'<link id1="{i}" id2="{normal_id}" r="3" g="238" b="255" radius="0.5"/>\n')
         cmm.write('</marker_set>\n')
     
     return None
-
 
 
