b8e8d233ad69135328e92a0777b393797f050ff3
Joseph Antinoro <jantinoro@Josephs-MacBook-Pro-2.local>
2026-05-11 12:58:56 +0100

    adjusted tomogram name handling

--- src/PickMe/utils/get_mgraph.py
+++ src/PickMe/utils/get_mgraph.py
@@ -10,7 +10,8 @@ def get_mgraph(segmentation_file_path):
     :type segmentation: str
     :return: Name of the .tomostar file
     :rtype: str
-    '''
+    /Users/jantinoro/Documents/LIDo/Rotation_2/python_projects/PickMe/outputs/choose/job009/1416_filtered_chosen.mrc.gz
+    ''' 
 
     if isinstance(segmentation_file_path, PosixPath):
         file_path_string = str(segmentation_file_path)
@@ -18,7 +19,11 @@ def get_mgraph(segmentation_file_path):
     else:
         path_parts = segmentation_file_path.split('/')
     segmentation_file = path_parts[-1]
-    mgraph_parts = segmentation_file.split('_')
-    mgraph_Name = mgraph_parts[0]+'_'+mgraph_parts[1]+mgraph_suffix
 
-    return mgraph_Name
+    #check if it is from object choice job
+    if "chosen" in segmentation_file:
+        mgraph_name = segmentation_file.strip('.mrc.gz')
+    else:
+        mgraph_parts = segmentation_file.split('_')
+        mgraph_name = mgraph_parts[0]+'_'+mgraph_parts[1]+mgraph_suffix
+    return mgraph_name
